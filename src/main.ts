dead1c58ff58554d891ac74f129d9caeebef3b44
Dean Jenkins <richarddeanjenkins@gmail.com>
2025-08-25 07:56:01 +0100

    Adding Air Quality link

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import openstreetmaplogo from '/OpenStreetMap-logo-with-text.svg'
 import ghplogo from '/gwchp-logo-coloured-600x600.png'
 import calendarlogo from '/calendar.svg'
 import acnylogo from '/acny.png'
+import airqualitylogo from '/PurpleAir-Cornwall-Map.png'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -17,6 +18,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <a href="https://www.achurchnearyou.com/search/?acny_search_place=Grampound%2C+Truro%2C+UK&lat=50.2992589&lon=-4.8984499&place=Grampound%2C+Truro+TR2%2C+UK&postcode=TR2&text=" target="_blank">
       <img src="${acnylogo}" class="logo" alt="A Church Near You logo" />
+    </a>
+    <a href="https://map.purpleair.com/air-quality-standards-us-epa-aqi?opt=%2F1%2Flp%2Fa10%2Fp604800%2FcC0#8.63/50.2076/-5.023" target="_blank">
+      <img src="${airqualitylogo}" class="logo" alt="Air Quality, PurpleAir logo" />
     </a>
     <h1>Grampound</h1>
     <p class="subtitle">
