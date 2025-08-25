16220e3dc3d269484dc5eadecbb74b990681f1d5
Dean Jenkins <richarddeanjenkins@gmail.com>
2025-08-25 07:56:44 +0100

    Shuffling order of links

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import airqualitylogo from '/PurpleAir-Cornwall-Map.png'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://photos.grampound.org.uk/" target="_blank">
-      <img src="${ghplogo}" class="logo green" alt="Grampound Heritage Project logo" />
-    </a>
     <a href="https://www.openstreetmap.org/#map=19/50.298719/-4.903400" target="_blank">
       <img src="${openstreetmaplogo}" class="logo vanilla" alt="OpenStreetMap logo" />
     </a>
     <a href="https://grampoundvillagehall.org.uk/calendar.php" target="_blank">
       <img src="${calendarlogo}" class="logo" alt="Grampound calendar logo" />
+    </a>
+    <a href="https://photos.grampound.org.uk/" target="_blank">
+      <img src="${ghplogo}" class="logo green" alt="Grampound Heritage Project logo" />
     </a>
     <a href="https://www.achurchnearyou.com/search/?acny_search_place=Grampound%2C+Truro%2C+UK&lat=50.2992589&lon=-4.8984499&place=Grampound%2C+Truro+TR2%2C+UK&postcode=TR2&text=" target="_blank">
       <img src="${acnylogo}" class="logo" alt="A Church Near You logo" />
