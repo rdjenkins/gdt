8a67e1678f3185e15aab3e772615df963593d1ea
Dean Jenkins <richarddeanjenkins@gmail.com>
2025-09-05 00:06:46 +0100

    Restyled the look.

--- src/main.ts
+++ src/main.ts
@@ -41,32 +41,67 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://www.openstreetmap.org/#map=19/50.298719/-4.903400" target="_blank">
-      <img src="${openstreetmaplogo}" class="logo vanilla" alt="OpenStreetMap logo" />
-    </a>
-    <a href="https://grampoundvillagehall.org.uk/calendar.php" target="_blank">
-      <img src="${calendarlogo}" class="logo" alt="Grampound calendar logo" />
-    </a>
-    <a href="https://www.achurchnearyou.com/search/?acny_search_place=Grampound%2C+Truro%2C+UK&lat=50.2992589&lon=-4.8984499&place=Grampound%2C+Truro+TR2%2C+UK&postcode=TR2&text=" target="_blank">
-      <img src="${acnylogo}" class="logo" alt="A Church Near You logo" />
-    </a>
-    <a href="https://photos.grampound.org.uk/" target="_blank">
-      <img src="${ghplogo}" class="logo green" alt="Grampound Heritage Project logo" />
-    </a>
-    <a href="https://stcrida.co.uk/cs.php" target="_blank">
-      <img src="${creedchurchyardlogo}" class="logo green" alt="Creed Churchyard logo" />
-    </a>
-    <a href="https://www.floodmapper.co.uk/data-explorer/search-sewage-report/1bfb1dc2-aaf8-11ee-baa2-0242ac140003/Grampound" target="_blank">
-      <img src="${waterqualitylogo}" class="logo" alt="Sewage discharges logo" />
-    </a>
-    <a href="https://map.purpleair.com/air-quality-standards-us-epa-aqi?opt=%2F1%2Flp%2Fa10%2Fp604800%2FcC0#8.63/50.2076/-5.023" target="_blank">
-      <img src="${airqualitylogo}" class="logo" alt="Air Quality, PurpleAir logo" />
-    </a>
-    <div id="search-container"></div>
     <h1>Grampound</h1>
     <p class="subtitle">
     digital twin project
     </p>
+
+    <ul class="flex-container">
+    <li class="flex-item">
+      <a href="https://www.openstreetmap.org/#map=19/50.298719/-4.903400" target="_blank">
+        <img src="${openstreetmaplogo}" class="logo vanilla" alt="OpenStreetMap logo" />
+      </a>
+      <p>Map of Grampound with Creed.</p>
+    </li>
+
+    <li class="flex-item">
+        <div id="search-container"></div>
+      <p>Search for an address or place in Grampound with Creed.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://grampoundvillagehall.org.uk/calendar.php" target="_blank">
+        <img src="${calendarlogo}" class="logo" alt="Grampound calendar logo" />
+      </a>
+      <p>Village Hall calendar of events.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://www.achurchnearyou.com/search/?acny_search_place=Grampound%2C+Truro%2C+UK&lat=50.2992589&lon=-4.8984499&place=Grampound%2C+Truro+TR2%2C+UK&postcode=TR2&text=" target="_blank">
+        <img src="${acnylogo}" class="logo" alt="A Church Near You logo" />
+      </a>
+      <p>Church services in Grampound with Creed.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://photos.grampound.org.uk/" target="_blank">
+        <img src="${ghplogo}" class="logo green" alt="Grampound Heritage Project logo" />
+      </a>
+      <p>Grampound Heritage Project photo archive.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://stcrida.co.uk/cs.php" target="_blank">
+        <img src="${creedchurchyardlogo}" class="logo green" alt="Creed Churchyard logo" />
+      </a>
+      <p>Creed Churchyard search.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://www.floodmapper.co.uk/data-explorer/search-sewage-report/1bfb1dc2-aaf8-11ee-baa2-0242ac140003/Grampound" target="_blank">
+        <img src="${waterqualitylogo}" class="logo" alt="Sewage discharges logo" />
+      </a>
+      <p>Water quality for the River Fal.</p>
+    </li>
+
+    <li class="flex-item">
+      <a href="https://map.purpleair.com/air-quality-standards-us-epa-aqi?opt=%2F1%2Flp%2Fa10%2Fp604800%2FcC0#8.63/50.2076/-5.023" target="_blank">
+        <img src="${airqualitylogo}" class="logo" alt="Air Quality, PurpleAir logo" />
+      </a>
+      <p>Air quality in Cornwall.</p>
+    </li>
+    
+    </ul>
   </div>
 `
 
