b96a2f0ced6af6830a61b37c7e6074ba37c2d302
Dean Jenkins <richarddeanjenkins@gmail.com>
2025-12-09 12:25:23 +0000

    Reversing the graceful PurpleAir failing

--- src/purpleair.ts
+++ src/purpleair.ts
@@ -1,7 +1,7 @@
 // code for PurpleAir widget functionality
 import { submitLog } from "./utils"
 
-const PURPLE_AIR_CHOICE = "Grampound"
+var PURPLE_AIR_CHOICE = "Grampound"
 const PURPLEAIR_TRURO_ID = 'PurpleAirWidget_262781_module_US_EPA_AQI_conversion_C0_average_10_layer_US_EPA_AQI'
 const PURPLEAIR_TRURO_URL = 'https://www.purpleair.com/pa.widget.js?key=DX82CA29U5Z4C6HO&module=US_EPA_AQI&conversion=C0&average=10&layer=US_EPA_AQI&container=PurpleAirWidget_262781_module_US_EPA_AQI_conversion_C0_average_10_layer_US_EPA_AQI'
 const PURPLEAIR_GRAMPOUND_ID = 'PurpleAirWidget_288300_module_US_EPA_AQI_conversion_C0_average_10_layer_US_EPA_AQI'
@@ -23,6 +23,7 @@ export function purpleAirSensorWidget() {
 // Append the PurpleAir script to the document body after DOMContentLoaded
 document.addEventListener('DOMContentLoaded', () => {
     const script = document.createElement('script');
+    script.id = NEAREST_PURPLEAIR_SENSOR_WIDGET_ID + '_script'
     script.src = NEAREST_PURPLEAIR_SENSOR_URL;
     document.body.appendChild(script);
 });
@@ -87,9 +88,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 //observer.disconnect();
             } else {
-                widgetDiv.innerHTML = "<button>Visit Purple Air Map</button>"
-                observer.disconnect() // TODO how would we then try another one?
-                submitLog(`PurpleAir response does not show an obvious Pm2.5 figure`)
+                // This below doesn't work if the Purple Air is delayed slightly.
+                //widgetDiv.innerHTML = "<button>Visit Purple Air Map</button>"
+                //                observer.disconnect() // TODO how would we then try another one?
+                //if (document.getElementById(NEAREST_PURPLEAIR_SENSOR_WIDGET_ID + '_script')) {
+                //    document.getElementById(NEAREST_PURPLEAIR_SENSOR_WIDGET_ID + '_script')?.remove
+                //}
+                // TODO also the PurpleAir script just keeps trying and changing this area of HTML
+                //submitLog(`PurpleAir response does not show an obvious Pm2.5 figure`)
             }
         }
     });
