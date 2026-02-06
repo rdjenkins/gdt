e67acb050aa9908244cce81dcf31315a9da02579
Dean Jenkins <richarddeanjenkins@gmail.com>
2026-02-06 14:45:45 +0000

    Build 0.9.5 - second submission to iOS (widget and haptics added to make it more like an 'app'), submitted to Android, published on web

---
+++ ios/App/widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  widget
+//
+//  Created by Richard Dean Jenkins on 05/02/2026.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ ios/App/widget/widgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Richard Dean Jenkins on 05/02/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+        widgetLiveActivity()
+    }
+}
